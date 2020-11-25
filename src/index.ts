6d4cdf9f764d6d88f5faa359849f26f8e85df2e5
Tvelve <sergey.rogach@yandex.ru>
2020-11-25 20:44:11 +0300

    Добавили ссылку в лог

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ import { promises } from "fs";
 
 // не надо:
 // npm i --save-dev @types/node
+const host = "localhost";
+const port = 3000;
 
 const server = createServer();
 
@@ -49,4 +51,6 @@ server.on("request", async (req: IncomingMessage, res: ServerResponse) => {
     }
 });
 
-server.listen(3000);
+server.listen(port, () => {
+    console.log(`http://${host}:${port}`);
+});;
